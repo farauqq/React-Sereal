d3acfbaf3813f1b302e19c9e1a9f50b5cfaf047b
farauq <maulanafarauq@gmail.com>
2022-11-20 14:43:04 +0700

    wip gallery

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../../assets/css/style-profile.css";
 import "../../assets/css/style.css";
+import soon from "../../assets/img/cmingsoon.png";
 
 function Gallery() {
   return (
@@ -13,6 +14,27 @@ function Gallery() {
           Go to class
         </Link>
       </div>
+      {/* <!-- Gallery --> */}
+      <div className="row">
+        <div className="col-lg-4 col-md-12 mb-4 mb-lg-0">
+          <img src={soon} className="w-100 shadow-1-strong rounded mb-4" alt="Cooming Soon" />
+
+          {/* <img src={soon} className="w-100 shadow-1-strong rounded mb-4" alt="Wintry Mountain Landscape" /> */}
+        </div>
+
+        <div className="col-lg-4 mb-4 mb-lg-0">
+          <img src={soon} className="w-100 shadow-1-strong rounded mb-4" alt="Cooming Soon" />
+
+          {/* <img src="http://gallery.gudskul.art/wp-content/uploads/2020/07/Publikasi-Pameran-Rusun-IG-Feed.jpg" className="w-100 shadow-1-strong rounded mb-4" alt="Boat on Calm Water" /> */}
+        </div>
+
+        <div className="col-lg-4 mb-4 mb-lg-0">
+          <img src={soon} className="w-100 shadow-1-strong rounded mb-4" alt="Cooming Soon" />
+
+          {/* <img src="http://gallery.gudskul.art/wp-content/uploads/2020/07/Di-rumah-tak-berarti-melemah.jpg" className="w-100 shadow-1-strong rounded mb-4" alt="Yosemite National Park" /> */}
+        </div>
+      </div>
+      {/* <!-- Gallery --> */}
     </>
   );
 }
